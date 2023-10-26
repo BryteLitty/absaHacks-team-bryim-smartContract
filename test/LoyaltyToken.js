c9b95aeea814f8ed44e2674dc324e8bd0cf4a842
Bright Kportiklah <littyofficials@gmail.com>
2023-10-26 01:53:30 +0000

    update test

--- test/LoyaltyToken.js
+++ test/LoyaltyToken.js
@@ -2,89 +2,112 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("LoyaltyToken", function () {
-  let LoyaltyToken;
-  let loyaltyToken;
-  let owner;
-  let user;
 
-  before(async function () {
-    [owner, user] = await ethers.getSigners();
-    LoyaltyToken = await ethers.getContractFactory("LoyaltyToken");
-  });
+  let loyaltyToken;
+  let owner, addr1, addr2;
 
   beforeEach(async function () {
-    loyaltyToken = await LoyaltyToken.deploy("MyLoyaltyToken", "LOY", owner.address);
+    [owner, addr1, addr2] = await ethers.getSigners();
+
+    const LoyaltyToken = await ethers.getContractFactory("LoyaltyToken");
+    loyaltyToken = await LoyaltyToken.deploy("Loyalty", "LTY", owner.address);    
   });
 
-  it("Should deploy the LoyaltyToken contract", async function () {
-    expect(await loyaltyToken.name()).to.equal("MyLoyaltyToken");
-    expect(await loyaltyToken.symbol()).to.equal("LOY");
-    expect(await loyaltyToken.owner()).to.equal(owner.address);
+  describe("Deployment", function () {
+
+    it("Should set the right owner", async function () {
+      expect(await loyaltyToken.owner()).to.equal(owner.address);
+    });
+
+    it("Should assign the total supply of tokens to the owner", async function () {
+      const ownerBalance = await loyaltyToken.balanceOf(owner.address);
+      expect(await loyaltyToken.totalSupply()).to.equal(ownerBalance);
+    });
   });
 
-  it("Should mint new tokens when called by the owner", async function () {
-    const initialBalance = await loyaltyToken.balanceOf(owner.address);
-    const amountToMint = 1000;
-    await loyaltyToken.connect(owner).mint(user.address, amountToMint);
+  describe("Scaling factor", function () {
 
-    const finalBalance = await loyaltyToken.balanceOf(user.address);
-    const finalOwnerBalance = await loyaltyToken.balanceOf(owner.address);
+    it("Should have the correct default scalingFactor", async function () {
+      expect(await loyaltyToken.scalingFactor()).to.equal(100);
+    });
 
-    expect(finalBalance).to.equal(amountToMint);
-    expect(finalOwnerBalance).to.equal(initialBalance.sub(amountToMint));
   });
 
-  it("Should add loyalty points when called by the owner", async function () {
-    const initialPoints = await loyaltyToken.loyaltyPoints(user.address);
-    const pointsToAdd = 100;
-    await loyaltyToken.connect(owner).addLoyaltyPoints(user.address, pointsToAdd);
+  describe("Loyalty points", function () {
 
-    const finalPoints = await loyaltyToken.loyaltyPoints(user.address);
-    expect(finalPoints).to.equal(initialPoints.add(pointsToAdd));
+    it("Should allow owner to add loyalty points to a user", async function () {
+      const points = 200;
+      await expect(loyaltyToken.connect(owner).addLoyaltyPoints(addr1.address, points))
+        .to.emit(loyaltyToken, "Transfer")
+        .withArgs(owner.address, addr1.address, 0); 
+
+      expect(await loyaltyToken.loyaltyPoints(addr1.address)).to.equal(points);
+    });
+
+    it("Should not allow non-owner to add loyalty points", async function () {
+      await expect(
+        loyaltyToken.connect(addr1).addLoyaltyPoints(addr1.address, 100)
+      ).to.be.revertedWith("Ownable: caller is not the owner");
+    });
+
+    it("Should allow a user to check their loyalty points", async function () {
+      const points = 500;
+      await loyaltyToken.connect(owner).addLoyaltyPoints(addr1.address, points);
+
+      expect(await loyaltyToken.connect(addr1).checkLoyaltyPoints())
+        .to.equal(points);
+    });
+
   });
 
-  it("Should redeem loyalty points for tokens", async function () {
-    const pointsToAdd = 100;
-    await loyaltyToken.connect(owner).addLoyaltyPoints(user.address, pointsToAdd);
+  describe("Redeeming", function () {
 
-    const initialUserBalance = await loyaltyToken.balanceOf(user.address);
-    const initialOwnerBalance = await loyaltyToken.balanceOf(owner.address);
+    beforeEach(async function () {
+      await loyaltyToken.connect(owner).addLoyaltyPoints(addr1.address, 1000);
+    });
 
-    const pointsToRedeem = 50;
-    await loyaltyToken.connect(user).redeemLoyaltyPoints(pointsToRedeem);
+    it("Should allow a user to redeem their loyalty points", async function () {
+      const pointsToRedeem = 500;
+      const expectedTokens = pointsToRedeem * 100;
 
-    const finalUserBalance = await loyaltyToken.balanceOf(user.address);
-    const finalOwnerBalance = await loyaltyToken.balanceOf(owner.address);
-    const finalUserPoints = await loyaltyToken.loyaltyPoints(user.address);
+      await expect(loyaltyToken.connect(addr1).redeemLoyaltyPoints(pointsToRedeem))
+        .to.emit(loyaltyToken, "Transfer")
+        .withArgs(owner.address, addr1.address, expectedTokens);
 
-    expect(finalUserBalance).to.equal(initialUserBalance.add(pointsToRedeem * loyaltyToken.scalingFactor));
-    expect(finalOwnerBalance).to.equal(initialOwnerBalance.sub(pointsToRedeem * loyaltyToken.scalingFactor));
-    expect(finalUserPoints).to.equal(pointsToAdd - pointsToRedeem);
+      expect(await loyaltyToken.loyaltyPoints(addr1.address)).to.equal(1000 - pointsToRedeem);
+      expect(await loyaltyToken.balanceOf(addr1.address)).to.equal(expectedTokens);
+    });
+
+    it("Should revert if insufficient loyalty points", async function () {
+      await expect(
+        loyaltyToken.connect(addr1).redeemLoyaltyPoints(1500)
+      ).to.be.revertedWith("Insufficient loyalty points");
+    });
+
+    it("Should revert if insufficient token balance in contract", async function () {
+      await loyaltyToken.connect(owner).transfer(owner.address, await loyaltyToken.balanceOf(owner.address));
+      await expect(
+        loyaltyToken.connect(addr1).redeemLoyaltyPoints(100)    
+      ).to.be.revertedWith("Contract does not have enough tokens to redeem");
+    });
+
   });
 
-  it("Should not allow redeeming more loyalty points than available", async function () {
-    const pointsToAdd = 100;
-    await loyaltyToken.connect(owner).addLoyaltyPoints(user.address, pointsToAdd);
-    const pointsToRedeem = 200;
+  describe("Minting", function () {
 
-    await expect(loyaltyToken.connect(user).redeemLoyaltyPoints(pointsToRedeem)).to.be.revertedWith("Insufficient loyalty points");
+    it("Should allow the owner to mint new tokens", async function () {
+      const amount = 1000;
+      await expect(loyaltyToken.connect(owner).mint(owner.address, amount))
+        .to.emit(loyaltyToken, "Transfer")
+        .withArgs(ethers.constants.AddressZero, owner.address, amount);
+    });
+
+    it("Should not allow non-owner to mint", async function () {
+      await expect(
+        loyaltyToken.connect(addr1).mint(addr1.address, 100)
+      ).to.be.revertedWith("Ownable: caller is not the owner");
+    });
+
   });
 
-  it("Should not allow redeeming when the contract doesn't have enough tokens", async function () {
-    const pointsToAdd = 100;
-    await loyaltyToken.connect(owner).addLoyaltyPoints(user.address, pointsToAdd);
-
-    const balanceOfOwner = await loyaltyToken.balanceOf(owner.address);
-    const pointsToRedeem = 5000;
-
-    await expect(loyaltyToken.connect(user).redeemLoyaltyPoints(pointsToRedeem)).to.be.revertedWith("Contract does not have enough tokens to redeem");
-  });
-
-  it("Should allow users to check their loyalty points balance", async function () {
-    const pointsToAdd = 100;
-    await loyaltyToken.connect(owner).addLoyaltyPoints(user.address, pointsToAdd);
-    const userPoints = await loyaltyToken.connect(user).checkLoyaltyPoints();
-
-    expect(userPoints).to.equal(pointsToAdd);
-  });
 });
